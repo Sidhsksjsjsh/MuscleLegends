40b488927e01489e9045e5e4d97ad7c5f6bbffd2
Fhri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2023-10-30 17:53:34 +0900

    Update Script.lua

--- SyncX/Script.lua
+++ SyncX/Script.lua
@@ -1,22 +1,26 @@
-jk = {}
+local OrionLib = loadstring(game:HttpGet("https://pastebin.com/raw/NMEHkVTb"))()
+
+local jk = {}
 for _, v in pairs(game:GetService("ReplicatedStorage").chestRewards:GetDescendants()) do
     if v.Name ~= "Light Karma Chest" or v.Name ~= "Evil Karma Chest" then
         table.insert(jk, v.Name)
     end
 end
 
-jk1 = {}
+local jk1 = {}
 for _, v in pairs(game:GetService("Workspace").mapCrystalsFolder:GetDescendants()) do
     if v:FindFirstChild("Crystal") then
         table.insert(jk1, v.Name)
     end
 end
-Rocks = {}
+
+local tbl_Rocks = {}
 for i, v in pairs(game:GetService("Workspace").machinesFolder:GetChildren()) do
     if v:FindFirstChild("Rock") then
         table.insert(Rocks, v.Name)
     end
 end
+
 function c()
     for i, v in pairs(game.Players.LocalPlayer.Backpack:GetChildren()) do
         if
@@ -32,56 +36,62 @@ function c()
     end
 end
 
-local library = loadstring(game:HttpGet("https://pastebin.com/raw/Uub92rmN"))()
-
-
-local Window = library:AddWindow("Sync X - Cheating Tool",
-    {
-        main_color = Color3.fromRGB(170, 0, 0),
-        min_size = Vector2.new(373, 433),
-        toggle_key = Enum.KeyCode.RightShift,
-    })
+local Window = OrionLib:MakeWindow({Name = "VIP Turtle Hub V3", HidePremium = false, SaveConfig = false, ConfigFolder = "TurtleFi"})
     
-local T1 = Window:AddTab("Main")
-local T2 = Window:AddTab("Crystal")
-local T3 = Window:AddTab("ESP")
-local T4 = Window:AddTab("Misc")
+local T1 = Window:MakeTab({Name = "Main",Icon = "rbxassetid://",PremiumOnly = false})
 
-T1:AddSwitch("auto farm", function(State)
+local T2 = Window:MakeTab({Name = "Crystal",Icon = "rbxassetid://",PremiumOnly = false})
+local T5 = Window:MakeTab({Name = "Rocks",Icon = "rbxassetid://",PremiumOnly = false})
+local T6 = Window:MakeTab({Name = "Brawl",Icon = "rbxassetid://",PremiumOnly = false})
+local T7 = Window:MakeTab({Name = "Chest",Icon = "rbxassetid://",PremiumOnly = false})
+local T3 = Window:MakeTab({Name = "ESP",Icon = "rbxassetid://",PremiumOnly = false})
+local T4 = Window:MakeTab({Name = "Misc",Icon = "rbxassetid://",PremiumOnly = false})
+
+local Settings = {
+	WorkOut = false,
+	AutoMob = false,
+	WorkOut1 = false,
+	Rocks = false,
+	personal = false,
+	Rebirth = false,
+	Hot = false,
+	Crystals = false,
+        AutoPets = false,
+        Stop = false,
+        Stop1 = false,
+        InfiniteJump = false,
+        Esp = false,
+        PlayerEsp = false,
+        Tracers = false,
+        EspNames = false,
+        Boxes = false,
+        Sorry = false
+}
+
+T1:AddToggle({
+Name = "Auto Farm",
+Default = false,
+Callback = function(State)
 Settings.WorkOut = State
-game:GetService("RunService").Stepped:connect(
-    function()
-        pcall(
-            function()
-                if Settings.WorkOut then
-                    game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
-                end
-            end
-        )
-    end
-)
-spawn(function()
-while Settings.WorkOut do
-    wait()
-    pcall(
-        function()
-            game:GetService("Players").LocalPlayer.muscleEvent:FireServer("rep")
-            c()
-            game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9e9,9e9,9e9)
-            if not Settings.WorkOut then
-                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0, 0, 0)
-            end
+while wait() do
+    if Settings.WorkOut == false then break end
+    game:GetService("Players").LocalPlayer.muscleEvent:FireServer("rep")
+    c()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(9e9,9e9,9e9)
+        if not Settings.WorkOut then
+         game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0, 0, 0)
         end
-    )
 end
-end)
-end)
+end})
 
-T1:AddSwitch("auto mob", function(State)
+T1:AddToggle({
+Name = "Punch Farm ?",
+Default = false,
+Callback = function(State)
 Settings.AutoMob = State
 spawn(function()
-while Settings.AutoMob do
-    wait()
+while wait() do
+    if Settings.AutoMob == false then break end
     pcall(
         function()
             for i, v in pairs(game:GetService("Workspace").enemyNPCs["Battle Island"]:GetChildren()) do
@@ -116,187 +126,240 @@ while Settings.AutoMob do
     )
 end
 end)
+end})
 
-end)
-T1:AddSwitch("auto durability", function(State)
+T5:AddDropdown({
+Name = "Select Rocks",
+Default = Rocks[1],
+Options = Rocks,
+Callback = function(object)
+	Settings.Rocks = object
+end})
+
+T5:AddToggle({
+Name = "Auto Durability",
+Default = false,
+Callback = function(State)
 Settings.WorkOut1 = State
-game:GetService("RunService").Stepped:connect(
-    function()
-        pcall(
-            function()
-                if Settings.WorkOut1 then
-                    game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
-                end
-            end
-        )
-    end
-)
 spawn(function()
-while Settings.WorkOut1 do
-    wait()
-    pcall(
-        function()
+while wait() do
+    if Settings.WorkOut1 == false then break end
+    pcall(function()
             game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame =
                 game:GetService("Workspace").machinesFolder[Rocks].Rock.CFrame
             game:GetService("Players").LocalPlayer.muscleEvent:FireServer("punch", "leftHand")
 
             game:GetService("Players").LocalPlayer.muscleEvent:FireServer("punch", "rightHand")
             game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack.Punch)
-        end
-    )
+        end)
 end
 end)
-end)
+end})
 
-
-
-local RocksList = T2:AddDropdown("Rocks", function(object)
-	Settings.Rocks = object
-end)
-
-RocksList:Add(Rocks)
-
-T1:AddSwitch("personal feature", function(State)
+T1:AddToggle({
+Name = "Personal Feature",
+Default = false,
+Callback = function(State)
 Settings.personal = State
-game:GetService("RunService").Stepped:connect(
-    function()
-        pcall(
-            function()
+while wait() do
+	if Settings.personal == false then break end
+	pcall(function()
                 if Settings.personal then
                     game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
                     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-8980, -30, -6049)
                     game:GetService("Players").LocalPlayer.muscleEvent:FireServer("punch", "leftHand")
                     game:GetService("Players").LocalPlayer.muscleEvent:FireServer("punch", "rightHand")
-                    game.Players.LocalPlayer.Character.Humanoid:EquipTool(
-                        game:GetService("Players").LocalPlayer.Backpack.Punch
-                    )
+                    game.Players.LocalPlayer.Character.Humanoid:EquipTool(game:GetService("Players").LocalPlayer.Backpack.Punch)
                 end
-            end
-        )
-    end
-)
-end)
+            end)
+	end
+end})
 
 
-T1:AddButton("no tool cooldown", function()
+T4:AddButton({
+Name = "No Tool Cooldown",
+Callback = function()
 c()
 game:GetService("Players").LocalPlayer.Backpack.Punch:FindFirstChildOfClass("NumberValue").Value = 0
-end)
+end})
 
-T1:AddSwitch("auto rebirth", function(State)
+T1:AddToggle({
+Name = "Auto Rebirth",
+Default = false,
+Callback = function(State)
 Settings.Rebirth = State
 spawn(function()
-while Settings.Rebirth do
-    wait(0.1)
+while wait() do
+    if Settings.Rebirth == false then break end
     game:GetService("ReplicatedStorage").rEvents.rebirthRemote:InvokeServer("rebirthRequest")
 end
 end)
-end)
- 
-T1:AddSwitch("auto join brawl", function(State)
+end})
+
+T6:AddSlider({
+Name = "Timer",
+Min = 0,
+Max = 20,
+Default = 2,
+Color = Color3.fromRGB(255,255,255),
+Increment = 1,
+ValueName = "Seconds",
+Callback = function(Value)
+_G.BrawlTimer = Value
+end})
+
+T6:AddToggle({
+Name = "Auto Join Brawl",
+Default = false,
+Callback = function(State)
 Settings.Hot = State
 spawn(function()
-while Settings.Hot do
-    wait(2)
+while wait(_G.BrawlTimer) do
+    if Settings.Hot == false then break end
     game:GetService("ReplicatedStorage").rEvents.brawlEvent:FireServer("joinBrawl")
 end
 end)
-end)
+end})
 
-T1:AddButton("get all chest", function()
+T6:AddSlider({
+Name = "Timer",
+Min = 0,
+Max = 20,
+Default = 2,
+Color = Color3.fromRGB(255,255,255),
+Increment = 1,
+ValueName = "Seconds",
+Callback = function(Value)
+_G.cTimer = Value
+end})
+
+T7:AddButton({
+Name = "Get All Chest",
+Callback = function()
 for i = 1, #jk do
-    wait(2)
+    wait(_G.cTimer)
     game:GetService("ReplicatedStorage").rEvents.checkChestRemote:InvokeServer(jk[i])
 end
+end})
 
-end)
-
-local cry = T2:AddDropdown("Crystal", function(object)
+T2:AddDropdown({
+Name = "Select Crystal",
+Default = jk1[1],
+Options = jk1,
+Callback = function(object)
 	Settings.Crystals = object
-end)
+end})
 
-cry:Add(jk1)
-
-T2:AddSwitch("auto crystal", function(State)
+T2:AddToggle({
+Name = "Auto Crystal",
+Default = false,
+Callback = function(State)
 Settings.AutoPets = State
 spawn(function()
-while Settings.AutoPets do
-    wait()
+while wait() do
+    if Settings.AutoPets == false then break end
     game:GetService("ReplicatedStorage").rEvents.openCrystalRemote:InvokeServer("openCrystal", jk1)
 end
 end)
+end})
+
+local OldNameCall = nil
+OldNameCall = hookmetamethod(game, "__namecall", function(self, ...)
+    local Args = {...}
+        if self.Name == "sellPetEvent" and Args[1] == "sellPet" and Settings.Stop == true then
+                return 
+        end
+	if self.Name == "rebirthRemote" and Args[1] == "rebirthRequest" and Settings.Stop1 == true then
+                return 
+	end
+    return OldNameCall(self, unpack(Args))
 end)
 
-T1:AddSwitch("anti delete pets", function(State)
+T4:AddToggle({
+Name = "Anti-Delete Pets",
+Default = false,
+Callback = function(State)
 Settings.Stop = State
-local OldNameCall = nil
-OldNameCall = hookmetamethod(game, "__namecall", function(self, ...)
-    local Args = {...}
-        if self.Name == "sellPetEvent" and Args[1] == "sellPet" and Settings.Stop then
-                return 
-        end
-    return OldNameCall(self, unpack(Args))
-end)
-end)
+end})
 
-T1:AddSwitch("anti rebirth", function(State)
+T4:AddToggle({
+Name = "Anti-Rebirth",
+Default = false,
+Callback = function(State)
 Settings.Stop1 = State
-local OldNameCall = nil
-OldNameCall = hookmetamethod(game, "__namecall", function(self, ...)
-    local Args = {...}
-        if self.Name == "rebirthRemote" and Args[1] == "rebirthRequest" and Settings.Stop1 then
-                return 
-        end
-    return OldNameCall(self, unpack(Args))
-end)
+end})
+
+game:GetService("UserInputService").JumpRequest:connect(function()
+if Settings.InfiniteJump == true then
+    game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):ChangeState("Jumping")
+end
 end)
 
-T1:AddSwitch("infinite jump", function(State)
+T4:AddToggle({
+Name = "Infinite Jump",
+Default = false,
+Callback = function(State)
 Settings.InfiniteJump = State
-game:GetService("UserInputService").JumpRequest:connect(
-    function()
-        if Settings.InfiniteJump then
-            game:GetService "Players".LocalPlayer.Character:FindFirstChildOfClass "Humanoid":ChangeState("Jumping")
-        end
-    end
-)
-end)
+end})
 
 local ESP = loadstring(game:HttpGet("https://raw.githubusercontent.com/1201for/V.G-Hub/main/Karrot-Esp"))()
 
 
-T3:AddSwitch("enable esp", function(State)
+T3:AddToggle({
+Name = "Enable ESP",
+Default = false,
+Callback = function(State)
     Settings.Esp = State
     ESP:Toggle(Settings.Esp)
-end)
+end})
 
-T3:AddSwitch("player esp", function(State)
+T3:AddToggle({
+Name = "Player ESP",
+Default = false,
+Callback = function(State)
     Settings.PlayerEsp = State
     ESP.Players = Settings.PlayerEsp
-end)
-T3:AddSwitch("tracers esp", function(State)
+end})
+
+T3:AddToggle({
+Name = "Tracers ESP",
+Default = false,
+Callback = function(State)
     Settings.Tracers = State
     ESP.Tracers = Settings.Tracers
-end)
-T3:AddSwitch("name esp", function(State)
+end})
+
+T3:AddToggle({
+Name = "Name ESP",
+Default = false,
+Callback = function(State)
     ESP.Names = Settings.EspNames
     Settings.EspNames = State
-end)
-T3:AddSwitch("boxes esp", function(State)
+end})
+
+T3:AddToggle({
+Name = "Boxes ESP",
+Default = false,
+Callback = function(State)
     Settings.Boxes = State
     ESP.Boxes = Settings.Boxes
-end)
+end})
 
-T4:AddSwitch("inviscam", function(State)
+T4:AddToggle({
+Name = "Inviscam",
+Default = false,
+Callback = function(State)
 Settings.Sorry = State
-if Settings.Sorry then
+if Settings.Sorry == true then
     game:GetService("Players").LocalPlayer.DevCameraOcclusionMode = "Invisicam"
 else
     game:GetService("Players").LocalPlayer.DevCameraOcclusionMode = "Zoom"
 end
-end)
+end})
 
-T4:AddButton("Serverhop", function()
+T4:AddButton({
+Name = "Serverhop",
+Callback = function()
 local PlaceID = game.PlaceId
 local AllIDs = {}
 local foundAnything = ""
@@ -368,9 +431,21 @@ end
 
 -- If you'd like to use a script before server hopping (Like a Automatic Chest collector you can put the Teleport() after it collected everything.
 Teleport() 
-end)
-T4:AddButton("Rejoin", function()
+end})
+
+T4:AddButton({
+Name = "Rejoin",
+Callback = function()
 game:GetService("TeleportService"):Teleport(game.PlaceId, game:GetService("Players").LocalPlayer) 
+end})
+
+game:GetService("RunService").Stepped:connect(function()
+if Settings.WorkOut then
+        game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
+   end
+if Settings.WorkOut1 then
+        game.Players.LocalPlayer.Character.Humanoid:ChangeState(11)
+   end
 end)
 
 for i,v in pairs(game:GetService("ReplicatedStorage").funs:GetChildren()) do
